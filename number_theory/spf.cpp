a0dcde2bbae2060492ff2370b29ac831ba133d9a
FatBat2001 <adhamekhalede@gmail.com>
2023-07-21 15:03:34 +0300

    modified spf

--- number_theory/spf.cpp
+++ number_theory/spf.cpp
@@ -15,19 +15,15 @@ int spf[MAXN];
 
 void sieve()
 {
-    spf[1] = 1;
-    for (int i = 2; i < MAXN; i++)
-        spf[i] = i;
-    for (int i = 4; i < MAXN; i += 2)
-        spf[i] = 2;
-    for (int i = 3; i * i < MAXN; i++) {
-        if (spf[i] == i) {
-            for (int j = i * i; j < MAXN; j += i)
-                if (spf[j] == j)
+    for (int i = 1; i < MAXN; i ++) spf[i] = i;
+    for (int i = 2; i * i < MAXN; i ++) {
+        if (spf[i] == i){
+            for (int j = i; j < MAXN; j += i)  {
+                if (spf[j] == j) 
                     spf[j] = i;
+            }
         }
     }
-
 }
 
 map<int, int> get_fac(int x)
